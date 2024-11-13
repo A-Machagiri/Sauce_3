c5e7c4acb4d302d73fee701e7988577ffe96908d
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-13 21:58:21 +0530

    Update PageObjects/Login/LoginPage_Actions.cs

--- PageObjects/Login/LoginPage_Actions.cs
+++ PageObjects/Login/LoginPage_Actions.cs
@@ -1,9 +1,22 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
+﻿using System;
+using OpenQA.Selenium;
+
 namespace PageObjects.Login
 {
     public partial class LoginPage
     {
+        public void EnterCredentials(string username, string password)
+        {
+            IWebElement usernameField = driver.FindElement(By.Id(LoginPage_Locators.UsernameLocator));
+            IWebElement passwordField = driver.FindElement(By.Id(LoginPage_Locators.PasswordLocator));
+            usernameField.SendKeys(username);
+            passwordField.SendKeys(password);
+        }
 
+        public void ClickLoginButton()
+        {
+            IWebElement loginButton = driver.FindElement(By.Id(LoginPage_Locators.LoginButtonLocator));
+            loginButton.Click();
+        }
     }
 }
